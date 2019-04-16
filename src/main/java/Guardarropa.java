938e61741f1111b5847f2d3b89c806c34be3dbfe
Francisco Cortez <franciscoandrescortez@gmail.com>
2019-04-16 13:26:20 -0300

    TERMINADO LAS COMBINACIONES

--- src/main/java/Guardarropa.java
+++ src/main/java/Guardarropa.java
@@ -10,7 +10,7 @@ import com.google.common.collect.Sets;
 public class Guardarropa {
 	
 	//HashSet<Prenda> prendas = new HashSet<Prenda>();
-	ArrayList<Prenda> prendas = new ArrayList<Prenda>();
+	Set<Prenda> prendas = new HashSet<Prenda>();
 	
 	public void agregarPrenda(Prenda unaPrenda){
 		prendas.add(unaPrenda);
@@ -23,15 +23,18 @@ public class Guardarropa {
 		this.agregarPrenda(new Prenda(Color.AZUL,Color.VERDE, new Tipo(Categoria.CALZADO), Material.ALGODON));
 		this.agregarPrenda(new Prenda(Color.AZUL,Color.VERDE, new Tipo(Categoria.ACCESORIO), Material.ALGODON));	
 		this.agregarPrenda(new Prenda(Color.VIOLETA,Color.AZUL, new Tipo(Categoria.SUPERIOR), Material.ALGODON));
+		this.agregarPrenda(new Prenda(Color.VIOLETA,Color.AZUL, new Tipo(Categoria.SUPERIOR), Material.ALGODON));
 	}
-		//HashSet<Prenda> = Atuendo
-	//HashSet<HashSet<Prenda>> = HashSet<Atuendo>
+		
 	public Set<Set<Prenda>> devolverAtuendos(){
-		//return new HashSet(this.prendas);
-		 Set<Set<Prenda>> atuendos = Sets.combinations(ImmutableSet.of(this.prendas.get(0),this.prendas.get(1),
-				this.prendas.get(2),this.prendas.get(3),this.prendas.get(4)), 4);
+		//esto esta hardcodeado lo de abajo esta mal xd 
+		//Set<Set<Prenda>> atuendos = Sets.combinations(ImmutableSet.of(this.prendas.get(0),this.prendas.get(1),
+		//		this.prendas.get(2),this.prendas.get(3),this.prendas.get(4)), 4);
+		Set<Set<Prenda>> atuendos = Sets.combinations(prendas, 4);
+		 
+		//Aca tiene todas las combinaciones y se encarga de devolver solo las validas
 		 atuendos = atuendos.stream().filter(x->this.todosDistintos(x)).collect(Collectors.toSet());
-	return atuendos;
+		 return atuendos;
 	}
 	private boolean todosDistintos(Set<Prenda> lista) {
 		boolean a = lista.stream().map(y->y.tipo.categoria).collect(Collectors.toSet()).contains(Categoria.SUPERIOR);
