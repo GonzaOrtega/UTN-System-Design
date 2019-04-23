68db970ee6cfd50c7d7a8144d35826c667c2be6f
kmanrique@est.frba.utn.edu.ar <kmanrique@est.frba.utn.edu.ar>
2019-04-23 15:51:29 -0300

    Terminando issue #6: dejo abierto el issue por si se les ocurre otro test pero ya estan todos modificados

--- src/test/java/PrendaTest.java
+++ src/test/java/PrendaTest.java
@@ -1,28 +1,27 @@
 import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 
 public class PrendaTest {
 
-	Usuario juan = new Usuario(198,"JFQ8");
-	Guardarropa armario = new Guardarropa();
-	PrendaBuilder builderCamisaRosa = new PrendaBuilder().conColorPrimario(Color.ROSA).conColorSecundario(Color.VERDE).conTipo(TipoPrenda.CamisaMangaLarga).conTela(Material.ALGODON);
-	Prenda camisaRosa = builderCamisaRosa.crearPrenda();
-	PrendaBuilder builderRemeraAzul = new PrendaBuilder().conColorPrimario(Color.AZUL).conColorSecundario(Color.VERDE).conTipo(TipoPrenda.Remera).conTela(Material.ALGODON);
-	Prenda remeraAzul = builderRemeraAzul.crearPrenda();
+	@Test (expected = TieneParametrosNulosException.class)
+	public void noSePuedeCrearPrendasConParametrosNulos() {
+		new PrendaBuilder().conTipo(TipoPrenda.Calza).crearPrenda();
+	}
 	
-	@Before
-	public void setUp() throws Exception {
-		juan.agregarGuardarropa(armario);
+	@Test (expected = MaterialNoPermitidoException.class)
+	public void noSePuedenCrearPrendasConMaterialesInconsistentes() {
+		new PrendaBuilder().conTipo(TipoPrenda.Remera).conColorPrimario(Color.AZUL).conTela(Material.CUERO).crearPrenda();
 	}
+	
 	@Test
-	public void testYaSeCargoLaPrendaSegun(){
-		juan.cargarPrenda(armario, camisaRosa);
-		assertTrue(camisaRosa.yaSeCargoLaPrendaSegun(juan));
+	public void resultaValidoLaCreacionDePrendasSinColorSecundario(){
+		Prenda remeraRosa = new PrendaBuilder().conColorPrimario(Color.ROSA).conTipo(TipoPrenda.Remera).conTela(Material.ALGODON).crearPrenda();
+		assertEquals(remeraRosa.colorSecundario,null);
 	}
-	@Test
-	public void testNoSeCargoPrenda(){
-		assertFalse(camisaRosa.yaSeCargoLaPrendaSegun(juan));
+	
+	@Test (expected = TieneParametrosNulosException.class)
+	public void noSePuedeAsignarLaTelaSinSaberElTipo(){
+		new PrendaBuilder().conColorPrimario(Color.ROSA).conTela(Material.ALGODON).conTipo(TipoPrenda.Remera).crearPrenda();
 	}
+	
 }
