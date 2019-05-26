91e0dd6b54388def27fd3c01a4b2008a8421b9d3
Francisco Cortez <franciscoandrescortez@gmail.com>
2019-05-26 14:29:16 -0300

    cambio con la temperatura si da abrigo o no

--- src/main/java/Guardarropa.java
+++ src/main/java/Guardarropa.java
@@ -7,31 +7,45 @@ import com.google.common.collect.Sets;
 
 public class Guardarropa {
 	Set<Prenda> prendas = new HashSet<Prenda>();
-	
+	int temperatura = 24; // Cambiar esto cuando tengamos la API del clima jeje
 	public void cargarPrenda(Prenda unaPrenda){
 		prendas.add(unaPrenda);
 	}
 	
 	public Set<Set<Prenda>> pedirAtuendos(){
 		Set<Set<Prenda>> atuendos = new HashSet<Set<Prenda>>();
-		if(prendas.size()<5) return atuendos;
-		atuendos = Sets.combinations(prendas, 5);
+		if(prendas.size()<cantidadPrendasSegunTemp(temperatura)) 
+			return atuendos;
+		atuendos = Sets.combinations(prendas, cantidadPrendasSegunTemp(temperatura));
 		//Aca tiene todas las combinaciones y se encarga de devolver solo las validas
 		 atuendos = atuendos.stream().filter(atuendo->this.contienePrendasDeTodasLasCategorias(atuendo)).collect(Collectors.toSet());
 		 return atuendos;
 	} 
+	private int cantidadPrendasSegunTemp(int temperatura) {
+		if(temperatura>20) 
+			return 4;
+		else 
+			return 5;
+	}
 	
 	private boolean contienePrendasDeCategoria(Set<Prenda> atuendo, Categoria unaCategoria) {
 		return atuendo.stream().anyMatch(prenda->prenda.getTipo().categoria == unaCategoria);
 	}
 	
 	private boolean contienePrendasDeTodasLasCategorias(Set<Prenda> atuendo) {
+		if (atuendo.size()==4) {
 		return this.contienePrendasDeCategoria(atuendo, Categoria.SUPERIOR)
 				&& this.contienePrendasDeCategoria(atuendo, Categoria.INFERIOR)
 				&& this.contienePrendasDeCategoria(atuendo, Categoria.ACCESORIO)
-				&& this.contienePrendasDeCategoria(atuendo, Categoria.CALZADO)
-				&& this.contienePrendasDeCategoria(atuendo, Categoria.ABRIGO);
-	}
+				&& this.contienePrendasDeCategoria(atuendo, Categoria.CALZADO);
+			}
+		else 
+			return this.contienePrendasDeCategoria(atuendo, Categoria.SUPERIOR)
+					&& this.contienePrendasDeCategoria(atuendo, Categoria.INFERIOR)
+					&& this.contienePrendasDeCategoria(atuendo, Categoria.ACCESORIO)
+					&& this.contienePrendasDeCategoria(atuendo, Categoria.CALZADO)
+					&& this.contienePrendasDeCategoria(atuendo, Categoria.ABRIGO);
+		}//Se puede delegar y evitar la repeticion pero soy re cabeza yo -Fran
 	
 	public int cantidadDePrendasGuardadas() {
 		return this.prendas.size();
