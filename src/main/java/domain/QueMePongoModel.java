474d1998e18d90319c724664001bd4fcb03fdcec
chiarama <37699529+chiarama@users.noreply.github.com>
2019-08-07 18:28:56 -0300

    ArregladoUnPoco

--- src/main/java/domain/QueMePongoModel.java
+++ src/main/java/domain/QueMePongoModel.java
@@ -1,19 +1,29 @@
 package domain;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import org.uqbar.commons.model.annotations.Observable;
 
+import domain.apisClima.MockAPI;
+import domain.apisClima.ProveedorClima;
+import domain.frecuenciasDeEventos.FrecuenciaUnicaVez;
+
 @Observable
 public class QueMePongoModel {
 	private int fechaInicio;
 	private int fechaFin;
-	private Set<Evento> eventos;
-	private String fecha = "hola";
-	public String getFecha() {
-		return fecha;
-	}
+	ProveedorClima APIDeMentiritas = new MockAPI(21,23,false);
+	Sugeridor sugeridor = new Sugeridor(APIDeMentiritas);
+	private Evento evento = new Evento(LocalDateTime.of(LocalDate.of(2019, Month.MAY, 24),LocalTime.now()),sugeridor,new FrecuenciaUnicaVez());
+	private Evento p = new Evento(LocalDateTime.of(LocalDate.of(2019, Month.MAY,23),LocalTime.now()),sugeridor,new FrecuenciaUnicaVez());
+	private Set<Evento> eventos= new HashSet<Evento>();
+	public QueMePongoModel() {
+		eventos .add(evento);
+	};
 	public int getFechaInicio() {
 		return fechaInicio;
 	}
@@ -30,27 +40,24 @@ public class QueMePongoModel {
 		this.fechaFin = fecha;
 	}
 
-	public Set<Evento> eventos(){
-		return RepositorioDeUsuarios.getInstance().eventos();
-	}
-
-	public Set<Evento> getEventos() {
+	public Set<Evento> getEventos(){
 		return eventos;
 	}
 
 	public void setEventos(Set<Evento> eventos) {
-		this.eventos = eventos;
+		this.eventos= eventos;
 	}
 
 	public LocalDateTime fecha(int fechaEnNro) {
 		return LocalDateTime.of(fechaEnNro/10000,(fechaEnNro%10000)/100,fechaEnNro/1000000,0,0,0);
 	}
 	public void listarEventos() {
-		eventos= RepositorioDeUsuarios.getInstance()
+		/*eventos= RepositorioDeUsuarios.getInstance()
 					.eventos()
 					.stream()
 					.filter(evento->evento.sucedeEntreEstasfechas(this.fecha(fechaInicio),this.fecha(fechaFin)))
-					.collect(Collectors.toSet());
+					.collect(Collectors.toSet());*/
+		eventos.add(p);
 	}
 	
 }
