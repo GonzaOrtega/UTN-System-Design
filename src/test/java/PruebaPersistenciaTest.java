7f27292e7a4cc33de4a64d6020c6aaebdc7b3cd9
FranciscoII <francortez993@gmail.com>
2019-09-10 14:31:41 -0300

    Test usuario persistido

--- src/test/java/PruebaPersistenciaTest.java
+++ src/test/java/PruebaPersistenciaTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import org.junit.Before;
@@ -11,11 +13,15 @@ import javax.persistence.EntityManager;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+
+import antlr.collections.List;
+
 
 public class PruebaPersistenciaTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+	EntityManager em = entityManager();
 	@Test
 	  public void testName2() throws Exception {
-		  EntityManager em = entityManager();
 		  Guardarropa bruno = new Guardarropa();
 		  Guardarropa giorno = new Guardarropa();
 		  em.persist(bruno);
@@ -32,4 +38,43 @@ public class PruebaPersistenciaTest extends AbstractPersistenceTest implements W
 		  Long nuevoId2 = paleta2.getId();
 		  assertNotEquals(nuevoId2,nuevoId);
 	}
+	@Test
+	  public void testUsuario() throws Exception {		  
+		Usuario bruno = new Usuario(TipoUsuario.PREMIUM,25);
+		Usuario giorno = new Usuario(TipoUsuario.GRATUITO,15);
+		Guardarropa soyElGuarda = new Guardarropa();
+		bruno.agregarGuardarropa(soyElGuarda);
+		em.persist(bruno);
+		em.persist(giorno);
+		ArrayList<Usuario> listaUsuariosPersistidos;
+		listaUsuariosPersistidos = (ArrayList<Usuario>) em
+				  .createQuery("from Usuario order by Id", Usuario.class)
+				  .getResultList();
+
+		  int cantidadUsuariosPers = listaUsuariosPersistidos.size();
+		  int numEsperado = 2;
+		  assertEquals(cantidadUsuariosPers, numEsperado);
+	}
+	@Test
+	  public void guardaBienLosAtributos() throws Exception {		  
+		Usuario koichi = new Usuario(TipoUsuario.GRATUITO,15);
+		Usuario luisito = new Usuario(TipoUsuario.PREMIUM,125);
+		Guardarropa soyElGuarda = new Guardarropa();
+		koichi.agregarGuardarropa(soyElGuarda);
+		em.persist(koichi);
+		em.persist(luisito);
+		Usuario koichiPersistido = em
+				  .createQuery("from Usuario order by Id", Usuario.class)
+				  .getResultList()
+				  .get(0);
+		Usuario luisitoPersistido = em
+				  .createQuery("from Usuario order by Id", Usuario.class)
+				  .getResultList()
+				  .get(1);
+		assertTrue(koichiPersistido.getGuardarropas()
+					.contains(soyElGuarda));
+		assertTrue(koichi == koichiPersistido);
+		assertFalse(koichi.getTipo() == luisito.getTipo());
+	}
+	
 }
--- src/test/java/db/ContextTest.java
+++ src/test/java/db/ContextTest.java
@@ -1,13 +1,18 @@
 package db;
 
 import static org.junit.Assert.*;
-
+import domain.*;
 import org.junit.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
-
+import javax.persistence.EntityManager;
 public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
-
+	Guardarropa diavolo = new Guardarropa();
+	EntityManager em = entityManager();
+	
+	@Test
+	public void nuevoTest() {
+	}
 	@Test
 	public void contextUp() {
 		assertNotNull(entityManager());
