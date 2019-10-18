7d184b36da20edb376264a528f32869ed81c1018
FranciscoII <francortez993@gmail.com>
2019-10-17 22:18:18 -0300

    Creacion de Router y layaout QueMePongo

--- src/main/java/server/InicioSesionController.java
+++ src/main/java/server/InicioSesionController.java
@@ -1,4 +1,7 @@
 package server;
+import java.util.HashMap;
+
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
@@ -7,7 +10,21 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 
 public class InicioSesionController {
-	public static boolean verificarUsuario(Request req,Response res) {
-		return true;
+	public static String verificarUsuario(Request req,Response res) {
+		String km = req.queryParams("km");
+		Double kmNumero;
+		if(km==null)
+			kmNumero = 0.0;
+		else
+			kmNumero = Double.parseDouble(km);
+		Boolean hayKm = km!= null;
+		Double millas = 2*(kmNumero);
+		HashMap<String,Object> viewModel = new HashMap();
+
+		viewModel.put("km",kmNumero);
+		viewModel.put("hayKm", hayKm);
+		viewModel.put("millas", millas);
+		ModelAndView modelAndView =	new ModelAndView(viewModel,"inicioSesion.hbs");
+		return new HandlebarsTemplateEngine().render(modelAndView);
 	}
 }
