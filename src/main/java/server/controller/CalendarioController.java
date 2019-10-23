bb1324babffe4ecec0929adc7a37ae7f60408617
chiarama <37699529+chiarama@users.noreply.github.com>
2019-10-23 19:37:57 -0300

    Arreglado el boton de sugerencias en calendario

--- src/main/java/server/controller/CalendarioController.java
+++ src/main/java/server/controller/CalendarioController.java
@@ -3,25 +3,17 @@ package server.controller;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import javax.persistence.EntityManager;
-
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import domain.Evento;
-import domain.Prenda;
-import domain.PrendaBuilder;
 import domain.RepositorioDeUsuarios;
 import domain.Sugerencia;
 import domain.Usuario;
-import domain.enums.Color;
-import domain.enums.Material;
 import domain.enums.TipoFrecuencia;
-import domain.enums.TipoPrenda;
-import domain.enums.TipoUsuario;
+import domain.enums.TipoSugerencias;
 import domain.frecuenciasDeEventos.FrecuenciaUnicaVez;
 import spark.ModelAndView;
 import spark.Request;
@@ -30,13 +22,12 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class CalendarioController implements WithGlobalEntityManager{
 	EntityManager em = entityManager();
-	//private List<Evento> eventos =new ArrayList<Evento>();
 	List<Evento> eventos=null; 
 	public ModelAndView verSugerencia(Request req, Response res) {
 		String numString=req.queryParams("evento");
 		int numEvento = Integer.parseInt(numString);
-		//Evento evento = eventos.get(numEvento);
-		//res.cookie("evento",evento.getId().toString());
+		Evento evento = eventos.get(numEvento);
+		res.cookie("evento",evento.getId().toString());
 		res.redirect("/sugerencias");
 		return null;
 	}
@@ -49,10 +40,22 @@ public class CalendarioController implements WithGlobalEntityManager{
 		List<Evento>eventoList = null;
 		eventoList = hayFecha? calcularEventos(dia,mes,anio,usuarie):null;
 		HashMap<String, Object> viewModel = new HashMap<>();
+		List<Sugerencia> haySugerenciasPendientes = tieneSugerenciasPendientes(eventoList,usuarie);
+		viewModel.put("haySugerencias", haySugerenciasPendientes);
 		viewModel.put("eventos", eventoList);
 		viewModel.put("hayFecha", hayFecha);
 		ModelAndView modelAndView = new ModelAndView(viewModel, "calendario.hbs");
+		eventos = eventoList;
 		return new HandlebarsTemplateEngine().render(modelAndView);
+	}
+	public boolean tieneSugerenciaPendiente(List<Evento> eventos,Sugerencia sugerencia) {
+		return eventos.stream().anyMatch(evento->evento==sugerencia.getEvento())
+				&&sugerencia.getEstado()==TipoSugerencias.PENDIENTE;
+	}
+	public List<Sugerencia> tieneSugerenciasPendientes(List<Evento> eventos,Usuario usuario) {
+		List<Sugerencia> sugerencias =usuario.getSugerencias();
+		return sugerencias.stream().filter(sugerencia->tieneSugerenciaPendiente(eventos,sugerencia))
+				.collect(Collectors.toList());
 	}
 	public List<Evento> agregarEventos(Set<Evento> sets){
 		List<Evento> list = new ArrayList<Evento>();
@@ -68,12 +71,16 @@ public class CalendarioController implements WithGlobalEntityManager{
 		}
 		
 	}
+	public boolean noTieneSugerenciaPendiente(Evento evento,Usuario usuario) {
+		return !usuario.getSugerencias().stream().anyMatch(sugerencia->sugerencia.getEvento()==evento);
+	}
 	private List<Evento> calcularEventos(String dia,String mes,String anio,Usuario usuarie){
 		int diaNum = Integer.parseInt(dia);
 		int mesNum = Integer.parseInt(mes);
 		int anioNum = Integer.parseInt(anio);
 		List<Evento> eventos= new ArrayList<Evento>(usuarie.eventos());
 		LocalDateTime fecha = LocalDateTime.of(anioNum,mesNum,diaNum,0,0,0);
+		eventos = eventos.stream().filter(evento->noTieneSugerenciaPendiente(evento,usuarie)).collect(Collectors.toList());
 		//agregarEvento(usuarie);
 		return eventos.stream()
 				.filter(evento->((evento.getFrecuencia() ==TipoFrecuencia.Unico) && sucedeEnEsteDia(fecha,evento)))
