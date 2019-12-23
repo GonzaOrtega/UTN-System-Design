266c3503c8faf87b3f3b42afb86c26fc48f4cc7b
GonzaOrtega <gonzalonortega@gmail.com>
2019-12-23 12:48:51 -0300

    Modifico Paths de Alta de Evento

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -33,7 +33,7 @@ public class Router {
 		SugerenciasPendientesController sugerenciasPendientesController = new SugerenciasPendientesController();
 		SugerenciasController sugerenciasController = new SugerenciasController();
 		EventoController eventoController = new EventoController();
-		Calendario2Controller cal2Controller = new Calendario2Controller();
+//		Calendario2Controller cal2Controller = new Calendario2Controller();
 		
 		Spark.get("/", loginController::show, engine);
 		Spark.post("/", loginController::login, engine);
@@ -43,11 +43,11 @@ public class Router {
 		Spark.post("/sugerencias/show/aceptadas", sugerenciasController::elegirSugerenciaAceptada, engine);
 		Spark.get("/sugerencias/calificar/aceptadas", sugerenciasController::verCalificarSugerencias, engine);
 		Spark.post("/sugerencias/calificar/aceptadas", sugerenciasController::calificarSugerencias, engine);
-		Spark.get("/eventos/alta", eventoController::mostrarAltaDeEvento, engine);
-		Spark.post("/eventos/alta", eventoController::elegirDescripcionYFrecuencia, engine);
-		Spark.get("/eventos/alta/horarios", eventoController::mostrarOpcionesDeFrecuencia, engine);
-		Spark.post("/eventos/alta/horarios", eventoController::completarFrecuencia, engine);
-		Spark.post("/eventos/alta/verificacion", eventoController::mostrarMensajeVerificacion, engine);
+		Spark.get("/eventos/nuevo", eventoController::mostrarAltaDeEvento, engine);
+		Spark.post("/eventos/nuevo", eventoController::elegirDescripcionYFrecuencia, engine);
+		Spark.get("/eventos/nuevo/horarios", eventoController::mostrarOpcionesDeFrecuencia, engine);
+		Spark.post("/eventos/nuevo/horarios", eventoController::completarFrecuencia, engine);
+		Spark.post("/eventos/nuevo/verificacion", eventoController::mostrarMensajeVerificacion, engine);
 		Spark.get("/calendario", calendarioController::verCalendario);
 		Spark.post("/calendario", calendarioController::verSugerencia);
 		Spark.get("/sugerenciasPendientes", sugerenciasPendientesController::verSugerencias);
@@ -61,7 +61,7 @@ public class Router {
 		
 		Spark.get("/guardarropas", guardarropasController::pru,engine);
 		
-		Spark.get("/calendar",cal2Controller::test,engine);
+//		Spark.get("/calendar",cal2Controller::test,engine);
 		
 		Spark.after((request, response) -> { 
 			   PerThreadEntityManagers.getEntityManager(); 
